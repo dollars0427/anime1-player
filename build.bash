fa9cdb813377edcae5d49780d0d06c3858861d0a
Sardo Ip <sardo@pixelactionstudio.com>
2019-08-26 11:54:50 +0800

    Remove -p param from build script

--- build.bash
+++ build.bash
@@ -3,4 +3,4 @@
 # Maintainer Mr.Twister
 
 path=`pwd`
-nativefier --name "Anime1" -p linux --inject $path/fullscreen.js --internal-urls ".*?\anime\.*?" --width 858 --height 531 --always-on-top anime1.me 
+nativefier --name "Anime1" --inject $path/fullscreen.js --internal-urls ".*?\anime\.*?" --width 858 --height 531 --always-on-top anime1.me 
